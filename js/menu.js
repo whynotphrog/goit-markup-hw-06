82996a98d1b058d5c9822239f6c707cad3b6684f
Oleh Shtembuliak <oleg.shtembulyack@gmail.com>
2025-02-18 21:53:40 +0800

    add all

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle("is-open");
+  }
+})();
